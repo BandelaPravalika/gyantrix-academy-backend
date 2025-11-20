97f559a7fe69c53e60f6807fe6728d38e18e797d
BandelaPravalika <bandelapravalika015@gmail.com>
2025-11-20 17:36:36 +0530

    updating controller

--- src/main/java/com/academy/management/controller/LeadAppController.java
+++ src/main/java/com/academy/management/controller/LeadAppController.java
@@ -1,14 +1,26 @@
 package com.academy.management.controller;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.academy.management.model.LeadApp;
 import com.academy.management.service.LeadAppService;
@@ -81,6 +93,18 @@ public class LeadAppController {
     @GetMapping("/test")
     public String test() {
         return "API is working!";
+    }
+
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("/db-test")
+    public String dbTest() {
+        try (Connection conn = dataSource.getConnection()) {
+            return "DB connected: " + conn.getCatalog();
+        } catch (Exception e) {
+            return "DB connection failed: " + e.getMessage();
+        }
     }
 
 } 
